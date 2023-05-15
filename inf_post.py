52f086de062a9896ab0f1794730a9f2e6f9e40a1
seif-khaled <seifkhaled2424@gmail.com>
2023-05-15 14:12:49 +0300

    added another algorthim

--- inf_post.py
+++ inf_post.py
@@ -2,6 +2,47 @@ import re
 import math 
 string_ex="2+((8+2*3)/2)-1"
 expected_output="2823*+2/+1-"
+##################################
+def evaluate_infix(string):
+    operators=['+','-','*','/','^']
+    parnatheises_stack=[]
+    parnatheises_stack_index=0
+    if (len(string)==0) or (string[0] in operators) or (string[len(string)-1] in operators):
+        return "invalid expression",False
+    else:
+        double_operator=0
+        same_operator=0
+        unequal_parantheises=0
+        for i in range(len(string)):
+            if i==len(string)-1:
+                break
+            if string[i]==string[i+1] and string[i] in operators:
+                same_operator=1
+                return "same operator found consecutively",False
+            elif string[i] in operators and string[i+1] in operators:
+                double_operator=1
+                return "double operator found consecutively",False
+        for i in range(len(string)) :
+            if i==len(string)-1 and len(parnatheises_stack)>0:
+                unequal_parantheises=1
+                return "unbalanced parantheises found",False
+                
+            if string[i]=='(':
+                parnatheises_stack.append(string[i])
+                parnatheises_stack_index+=1
+            elif string[i]==')':
+                if len(parnatheises_stack)!=0:
+                    parnatheises_stack.pop(parnatheises_stack_index-1)
+                    parnatheises_stack_index-=1
+                elif len(parnatheises_stack)==0:
+                    unequal_parantheises=1
+                    return "unbalanced parantheises found",False
+        if unequal_parantheises==0 and same_operator==0 and double_operator==0:
+            return "expression is valid",True 
+                
+                                
+# print(evaluate_infix("/(/(1*2)-*"))
+
 #################################
 def infix_to_postfix(string):
     stack=[]
@@ -51,13 +92,11 @@ def infix_to_postfix(string):
                 stack_index-=1
         elif (string[index] not in operators) and (string[index]!='(' and string[index]!=')') :
              if (index -last_operand_index)>1:
-                 print("diffrence betwene operands is huge",string[index])
                  postfix_expression+=" "
                  postfix_expression+=string[index]
                  postfix_expression+" "
                  last_operand_index=index
              elif last_operand_index==0 or (index -last_operand_index)==1:
-                print("diffrencer betwen operands is okay",string[index])
                 postfix_expression+=string[index]
                 last_operand_index=index
         elif string[index] in operators:
@@ -104,9 +143,14 @@ def infix_to_postfix(string):
 # print(infix_to_postfix("2+((8+2*3)/2)-1"))
 # print(infix_to_postfix(("a+b*(c^d-e)^(f+g*h)-i")))
 # print(infix_to_postfix("a^bvc"))
+# print(infix_to_postfix("A*(B+C)/D"))
 # print(infix_to_postfix("(~a^~b)v(~c)"))
-print(infix_to_postfix("~((a^b)^(fvcc))"))
+# print(infix_to_postfix("~((a^b)^(fvcc))"))
+print(infix_to_postfix("(~a^c)vf ^ ((xvyvz)^(~avy))"))
 # print(infix_to_postfix("15+20*30"))
+# print(infix_to_postfix("((A+B)-C*(D/E))+F"))
+# print(infix_to_postfix("((A + Bver) - C * (D / E)) + F"))
+# print(infix_to_postfix("((F+R)-(p^(a-s)))"))
 # print(infix_to_postfix("11^(12^13)"))
 # print(("1 2    3 4     5").split())
 ############################################
@@ -118,3 +162,7 @@ print(infix_to_postfix("~((a^b)^(fvcc))"))
 #         if post_string[index] not in operators:
 #             stack.append()
 #         index+=1
+
+# x=[1,2,3,4,2,2]
+# x.remove(2)
+# print(x)
