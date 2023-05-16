9b3bab49090ed7b47f1be358c36782f8012d252d
seif-khaled <seifkhaled2424@gmail.com>
2023-05-16 15:34:15 +0300

    added pascal triangle

---
+++ pascal's triangle.py
@@ -0,0 +1,23 @@
+import math 
+import random 
+
+def genrate_nth_pascal_triangle(N):
+    x=[]
+    for i in range(N):
+        x.append([])
+    column=1
+    for i in range(N):
+        for j in range(column):
+            if j==0 or i==j:
+                x[i].append(1)
+                
+            else:
+                x[i].append(x[i-1][j-1]+x[i-1][j])
+        column+=1
+    return x
+        
+print(genrate_nth_pascal_triangle(30))    
+
+
+
+    
